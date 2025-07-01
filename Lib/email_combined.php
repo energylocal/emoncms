9202e7343f7a998b03b354159d28ea7ef891adc0
photekq <photekq@gmail.com>
2025-07-01 15:52:04 +0100

    Symfony diag

--- Lib/email_combined.php
+++ Lib/email_combined.php
@@ -38,8 +38,6 @@ class Email {
     function __construct() {
         global $settings, $linked_modules_dir, $USE_SYMFONY_MAILER, $MIRROR_TO_SYMFONY;
         $this->log = new EmonLogger(__FILE__);
-        $MIRROR_TO_SYMFONY = true;
-        $USE_SYMFONY_MAILER = false;
         $this->log->info("USE_SYMFONY_MAILER: " . ($USE_SYMFONY_MAILER ? 'true' : 'false'));
         $this->log->info("MIRROR_TO_SYMFONY: " . ($MIRROR_TO_SYMFONY ? 'true' : 'false'));
 
