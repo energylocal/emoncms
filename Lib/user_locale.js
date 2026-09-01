d70051e47dcc60899c60142569ae7422c472bce7
Luke Farrar <luke@energylocal.org.uk>
2026-09-01 14:24:42 +0100

    Merge fix 3

--- Lib/user_locale.js
+++ Lib/user_locale.js
@@ -1,29 +1,36 @@
 (function(user, callback){
-    if (!user) {
-        if (typeof callback === 'function') callback();
-        return;
+    if (!user) return;
+
+    // rework to fit the momentjs naming scheme for the locale files
+    momentjs_locales = {
+        da_DK:'da',
+        nl_BE:'nl-be',
+        nl_NL:'nl',
+        en_GB:'en-gb',
+        et_EE:'et',
+        fr_FR:'fr',
+        de_DE:'de',
+        it_IT:'it',
+        es_ES:'es',
+        cy_GB:'cy'
     }
-
-    var localeAliases = {
-        da_DK: 'da-DK',
-        nl_BE: 'nl-BE',
-        nl_NL: 'nl-NL',
-        en_GB: 'en-GB',
-        et_EE: 'et-EE',
-        fr_FR: 'fr-FR',
-        de_DE: 'de-DE',
-        it_IT: 'it-IT',
-        es_ES: 'es-ES',
-        cy_GB: 'cy-GB'
-    };
-
-    var locale = localeAliases[user.lang] || String(user.lang || 'en_GB').replace('_', '-');
-    user.locale = locale;
-
-    if (typeof document !== 'undefined' && document.documentElement) {
-        document.documentElement.lang = locale;
+    // match supported locales with momentjs file names
+    user.locale = momentjs_locales.hasOwnProperty(user.lang) ? momentjs_locales[user.lang] : 'en-gb'
+    // load the moment js locale file for the user's language (only if moment.js is present)
+    if (typeof moment === 'undefined') return;
+    var script = document.createElement('script');
+    script.src = path + "Lib/momentjs-locales/%s.js".replace("%s", user.locale);
+    script.id = '__user_loaded_locale';
+    if(typeof callback == 'function') {
+        script.addEventListener("load", function(event) {
+            // Some browsers can fire the load handler before the parser has
+            // finished the following inline scripts when this file is cached.
+            // Defer the callback so page globals such as graph's `_lang` exist.
+            setTimeout(function() {
+                callback(event);
+            }, 0);
+        });
     }
-
-    if (typeof callback === 'function') callback();
+    document.getElementsByTagName("script")[0].parentNode.appendChild(script);
 
 })(typeof _user !== 'undefined' ? _user: null, typeof _locale_loaded !== 'undefined' ? _locale_loaded: null);
